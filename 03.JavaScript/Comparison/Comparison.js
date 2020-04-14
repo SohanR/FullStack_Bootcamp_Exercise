aee58b8a347474c399c01b665656f7cae23cf2b4
SohanR <mizan.rahman66d@gmail.com>
2020-04-15 02:28:24 +0600

    ternary operator

---
+++ 03.JavaScript/Comparison/Comparison.js
@@ -0,0 +1,7 @@
+var counter = 10;
+
+//conditional (ternary) Operator
+var resulValue = counter < 10 ? 10 : 0;
+
+document.write("Compared: " + counter + "<br>");
+document.write("Result Value: " + resulValue + "<br>");
