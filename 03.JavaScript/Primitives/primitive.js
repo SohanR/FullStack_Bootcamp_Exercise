e6b0f6357355f39734759d2cef324cc2e3e746e0
SohanR <mizan.rahman66d@gmail.com>
2020-04-15 01:44:59 +0600

    data type

---
+++ 03.JavaScript/Primitives/primitive.js
@@ -0,0 +1,21 @@
+//Numeric data type
+
+var counter = 0;
+counter = counter + 100;
+console.log("counter: " + counter);
+
+//Decimal
+
+var decimalcount = 1.45;
+decimalcount = decimalcount + 74.32;
+console.log("decimalcount: " + decimalcount);
+
+//String
+var fullname = "Sohan";
+fullname = fullname + " D ";
+console.log("Fullname: " + fullname);
+
+//Boolean
+
+var flag = true;
+console.log("Flag valu: " + flag);
