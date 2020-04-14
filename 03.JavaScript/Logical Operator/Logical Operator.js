1baa1f683a2e9682be3d1fcd9363c3fdfcf64b4e
SohanR <mizan.rahman66d@gmail.com>
2020-04-15 03:10:06 +0600

    true or false

---
+++ 03.JavaScript/Logical Operator/Logical Operator.js
@@ -0,0 +1,6 @@
+var input1 = prompt("Enter First Value");
+var input2 = prompt("Enter second value");
+
+var result = input1 < input2 || input1 == input2;
+
+document.write(input1 + "<=" + input2 + " is " + result);
