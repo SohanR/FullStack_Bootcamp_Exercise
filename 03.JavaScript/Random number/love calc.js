0dd4ef602bfe51d413bd265264d4b2cfc6b8aac3
SohanR <mizan.rahman66d@gmail.com>
2020-04-16 12:51:19 +0600

    Math.random()

---
+++ 03.JavaScript/Random number/love calc.js
@@ -0,0 +1,8 @@
+var you = prompt("Your name?");
+var crash = prompt("Your crash name?");
+var n = 10;
+var n = Math.random() * 100;
+
+n = Math.floor(n) + 1;
+
+document.write(you + " and " + crash + " love score is " + n + "%");
